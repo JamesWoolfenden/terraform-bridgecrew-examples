26e7b48520b6119ba586b0418c6be708a201718f
James Woolfenden <james.woolfenden@gmail.com>
2021-11-12 16:17:20 +0000

    fix versions

--- example_provider/provider.bridgecrew.tf
+++ example_provider/provider.bridgecrew.tf
@@ -1,6 +1,6 @@
 provider "bridgecrew" {
   # don't add your secrets to files that get put into git
-  # this is isn't a valid api token so don't' try using it.
+  # this is isn't a valid api token so don't try using it.
   #url   = ""
   token = "ee60a054-b8f9-4b44-bc4e-e5f49d5bca5c"
 }
